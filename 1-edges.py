fd819829e085f40dba419ac1385a291151e3244a
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-20 10:51:21 +0200

    Python changes

--- 1-edges.py
+++
@@ -1,8 +0,0 @@
-word = "Holberton"
-word_first_3= "Hol"
-word_last_2="on"
-middle_word="olberto"
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print("First 3 letters: {}".format(word_first_3))
-print("Last 2 letters: {}".format(word_last_2))
-print("Middle word: {}".format(middle_word))
