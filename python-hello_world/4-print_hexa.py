cf0e947d6a5725985a16fa2fc2cf109d1cd5c298
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-21 00:13:10 +0200

    hexadecimal

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -3,5 +3,6 @@ for num in range(99):
         decimal_format = f"{num:2}"  
         hexadecimal_format = f"{num:02x}"  
 
-        print(f"{decimal_format} = 0x{hexadecimal_format}")
+       # print(f"{decimal_format} = 0x{hexadecimal_format}")
 
+print ( "{}=0x{}" .format(decimal_format, hexadecimal_format))
