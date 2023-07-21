dd4d98b4cf411c473581f1bc548ae825f987613a
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-21 23:33:38 +0200

    counting in python

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,3 +1,4 @@
+
 for digit2 in range(10):
     for digit3 in range(digit2 + 1, 10):
-        print(f"{digit2}{digit3:01d}", end=", " if digit2 < 9 or digit3 < 9 else "\n")
+        print(f"{digit2}{digit3:01d}" .format(digit2,digit3), end=", ")
