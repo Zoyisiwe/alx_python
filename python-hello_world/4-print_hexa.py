fc725a2eedcfd73de7bda7c01f6c8c5c7a3404e8
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-21 13:11:57 +0200

    Asending order  in python

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,8 +1,5 @@
 
 for num in range(99): 
         decimal_format = f"{num:2}"  
-        hexadecimal_format = f"{num:02x}"  
-
-       # print(f"{decimal_format} = 0x{hexadecimal_format}")
-
+        hexadecimal_format =f"{num:02x}"      
 print ( "{}=0x{}" .format(decimal_format, hexadecimal_format))
