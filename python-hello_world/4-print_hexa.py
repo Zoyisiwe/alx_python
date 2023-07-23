695ca01ddddb50da54540667ec5673ad95050136
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-23 15:57:53 +0200

    hexa

--- python-hello_world/4-print_hexa.py
+++ python-hello_world/4-print_hexa.py
@@ -1,8 +1,5 @@
 
-#for num in range(99): 
-        #decimal_format = f"{num:2}"  
-        #hexadecimal_format =f"{num:02x}"      
-        #print ( "{}=0x{}" .format(decimal_format, hexadecimal_format))
-
-for i in range(99):
-    print("{:02d}= {}".format(i, hex(i)))
+for num in range(99): 
+        decimal_format = f"{num:2}"  
+        hexadecimal_format =f"{num:02x}"      
+print ( "{}=0x{}" .format(decimal_format, hexadecimal_format))
