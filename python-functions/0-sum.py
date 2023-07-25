929d31bfa983d49c4d9636110b4bec3fb3c86c79
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-25 15:05:03 +0200

    a+b

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,14 +1,3 @@
-"""def add(a, b):
-    # Keep adding 'b' to 'a' until 'b' becomes 0
-    while b != 0:
-        carry = a & b  # Calculate the carry using bitwise AND
-        a = a ^ b      # Calculate the sum without considering the carry using bitwise XOR
-        b = carry << 1  # Shift the carry one position to the left
-    return a
-
-# Test the function
-result = add(5, 7)
-print(result)  # Output: 12"""
 
 #!/usr/bin/env python3
 #add = __import__('0-sum').add
