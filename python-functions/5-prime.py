c0a3c2f9f159579088c657afffe0c9b440967464
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-25 22:22:21 +0200

    prime

---
+++ python-functions/5-prime.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#is_prime = __import__('5-prime').is_prime
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
