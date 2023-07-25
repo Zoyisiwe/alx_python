d70a6a06714b234f5cc70622641b17431377ce13
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-25 21:51:59 +0200

    a^b

--- python-functions/1-power.py
+++ python-functions/1-power.py
@@ -2,11 +2,11 @@
 #pow = __import__('1-power').pow
 
 def pow(a,b):
-    return a^b
-print(pow(2, 2))
-print(pow(-2, 2))
-print(pow(10,-2))
-print(pow(-98, -10))
+    return a**b
+#print(pow(2, 2))
+#print(pow(-2, 2))
+#print(pow(10,-2))
+#print(pow(-98, -10))
 #print(pow(-4, 5))
 
 #pow(0,0)
