ed1f0a732ad4315651e5b8c8b8429f17b5a5a1ef
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-26 19:38:33 +0200

    add

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
 if __name__=="__main__":
 
  from add_0 import add
 
-a=1
-b=2
+ a=1
+ b=2
 
-result=add(a,b)
-print("{}+{}={}" .format(a,b,result))
+ result=add(a,b)
+
+ print("{}+{}={}" .format(a,b,result))
