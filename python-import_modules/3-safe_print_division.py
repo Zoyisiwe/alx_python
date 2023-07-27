f39ce81e826aa6277ac538c8a3472a447cf8fbd9
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-27 14:45:43 +0200

    division

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
         result = a / b
 
     except 0:
-        print("{:d} / {:d} = {}" .format(a, b, result))
+        print("Inside result:None")
 
         return None
     
