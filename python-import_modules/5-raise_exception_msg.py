1832c06de2897ea3d746556d168a59d506f847ee
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-27 15:28:49 +0200

    msg

--- python-import_modules/5-raise_exception_msg.py
+++ python-import_modules/5-raise_exception_msg.py
@@ -1,7 +1,13 @@
 def raise_exception_msg(message=""):
     raise NameError(message)
 
-try:
+'''try:
     raise_exception_msg("C is fun")
 except NameError as e:
-    print(f"Error: {e}")
+    print(f"Error: {e}")'''
+
+try:
+    message = "C is fun"
+    raise_exception_msg(message)
+except NameError as ne:
+    print(ne)
