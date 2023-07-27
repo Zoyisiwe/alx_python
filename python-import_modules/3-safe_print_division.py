16c944027579bf5cb82ba1866de0894ff7531b2f
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-27 14:36:14 +0200

    division

--- python-import_modules/3-safe_print_division.py
+++ python-import_modules/3-safe_print_division.py
@@ -3,9 +3,9 @@ def safe_print_division(a, b):
     try:
         result = a / b
 
-    except ZeroDivisionError:
+    except 0:
         print("Error: Division by zero.")
-        
+
         return None
     
     finally:
