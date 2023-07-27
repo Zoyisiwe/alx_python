2935936f5614d7e5a64b5bd70c1431e623dfa960
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-27 12:05:18 +0200

    args

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,20 @@
+import sys
+
+def print_arguments():
+    arguments = sys.argv[1:]  # Exclude the script name from the arguments list
+    num_arguments = len(arguments)
+
+    print(f"Number of argument(s): {num_arguments}", end="")
+
+    if num_arguments == 0:
+        print(".")
+    elif num_arguments == 1:
+        print(", followed by:")
+    else:
+        print("s, followed by:")
+
+    for i, arg in enumerate(arguments, start=1):
+        print(f"{i}: {arg}")
+
+if __name__ == "__main__":
+    print_arguments()
