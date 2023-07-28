115d381254cc752c097cf6930b7b59ac6bee4787
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-28 16:01:05 +0200

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,10 +1,10 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for i in range (0,len(row)):
-            if i == len(row) -1:
+        for i in range (0, len(row)):
+            if i == len(row) - 1 :
                 print("{:d}" .format(row[i]))
             else:
-                print("{:d}" .format(row[i]),end="") 
+                print("{:d}" .format(row[i]), end=" ") 
 
 print()
 
