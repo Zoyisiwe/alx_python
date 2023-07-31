09cd650209b09dad878879ef1c844d23433b5415
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-31 16:26:20 +0200

    common elements

---
+++ python-more_data_structures/1-common_elements.py
@@ -0,0 +1,9 @@
+def common_elements(set_1, set_2):
+    return set_1.intersection(set_2)
+
+# Test the function
+'''set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+result = common_elements(set1, set2)
+print(result)  # Output: {4, 5}'''
