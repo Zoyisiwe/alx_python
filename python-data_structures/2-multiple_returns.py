d856d962f2fc204eb425bbb0ea904bbd7244bf27
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-31 12:07:28 +0200

    multiple return

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -2,6 +2,9 @@ def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else None
     return length, first_char
+sentence=""
+length, first = multiple_returns(sentence)
+print("Length: {:d} - First character: {}".format(length, first))
 print()
 
 # Test the function
