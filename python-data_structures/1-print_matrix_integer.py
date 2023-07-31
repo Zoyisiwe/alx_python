8f4e4be0d47f5c6ff00ac44d57a4e7cf5f8557a2
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-31 11:28:19 +0200

    matrix

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -9,10 +9,10 @@ def print_matrix_integer(matrix=[[]]):
 print()
 
 # Test the function
-matrix = [
+'''matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
 
-print_matrix_integer(matrix)
+print_matrix_integer(matrix)'''
