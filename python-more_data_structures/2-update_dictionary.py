ebce2ce409859f2161679b889be618d3157b9b7c
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-31 16:38:10 +0200

    dict

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,13 @@
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+
+# Test the function
+'''my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Updating an existing key
+update_dictionary(my_dict, 'b', 10)
+print(my_dict)  # Output: {'a': 1, 'b': 10, 'c': 3}
+
+# Adding a new key
+update_dictionary(my_dict, 'd', 100)
+print(my_dict)  # Output: {'a': 1, 'b': 10, 'c': 3, 'd': 100}'''
