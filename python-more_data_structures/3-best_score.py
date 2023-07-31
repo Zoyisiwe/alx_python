37180e3ff1b9e20df8bec6144896216d737d8a65
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-31 17:04:21 +0200

    best score

--- python-more_data_structures/3-best_score.py
+++ python-more_data_structures/3-best_score.py
@@ -2,10 +2,10 @@ def best_score(a_dictionary):
     if not a_dictionary:
         return None
 
-    '''best_key = max(a_dictionary, key=a_dictionary.get)
+    best_key = max(a_dictionary, key=a_dictionary.get)
     return best_key
 
 # Test the function
-scores = {'Alice': 85, 'Bob': 92, 'Charlie': 78, 'David': 95}
+'''scores = {'Alice': 85, 'Bob': 92, 'Charlie': 78, 'David': 95}
 result = best_score(scores)
 print(result)  # Output: 'David' '''
