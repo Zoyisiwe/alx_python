0e3f34646058e5f9cfdd5960e9d8df365709c6b3
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-31 12:03:34 +0200

    multiple return

---
+++ python-data_structures/2-multiple_returns.py
@@ -0,0 +1,14 @@
+def multiple_returns(sentence):
+    length = len(sentence)
+    first_char = sentence[0] if length > 0 else None
+    return length, first_char
+print()
+
+# Test the function
+'''result1 = multiple_returns("Hello, World!")
+result2 = multiple_returns("Python")
+result3 = multiple_returns("")
+
+print(result1)  # Output: (13, 'H')
+print(result2)  # Output: (6, 'P')
+print(result3)  # Output: (0, None)'''
