cdf1934ee5e820b65683a8ca252cc5bb25cfd08a
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-07-31 12:43:53 +0200

    more returns

--- python-data_structures/2-multiple_returns.py
+++ python-data_structures/2-multiple_returns.py
@@ -1,8 +1,8 @@
-def multiple_returns(sentence):
+'''def multiple_returns(sentence):
     length = len(sentence)
     first_char = sentence[0] if length > 0 else None
     return length, first_char
-print()
+print()'''
 
 '''sentence = ""
 length, first = multiple_returns(sentence)
@@ -17,3 +17,16 @@ result3 = multiple_returns("")
 print(result1)  # Output: (13, 'H')
 print(result2)  # Output: (6, 'P')
 print(result3)  # Output: (0, None)'''
+
+def multiple_returns(sentence):
+    if sentence:
+        return len(sentence), sentence[0]
+    else:
+        return 0, None
+    print()
+
+# Test the function
+'''result1 = multiple_returns("Hello, world!")
+result2 = multiple_returns("")
+print(result1)  # Output: (13, 'H')
+print(result2)  # Output: (0, None)'''
