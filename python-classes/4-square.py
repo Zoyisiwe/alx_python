e4908cabf033b060ef7ad05ed1896834d86d7361
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-02 10:15:07 +0200

    Square5

---
+++ python-classes/4-square.py
@@ -0,0 +1,33 @@
+"""This class  is based and worked upon the file 3-square"""
+
+class Square:
+    """This class has a private instance attribute
+       with the value size that must be a integer and not less than 0 
+       with the area of the square, and a property setter, and a added
+       public instance method that prints in stdout.
+    """
+    def __init__(self, size=0):
+        self.size = size  # Call the property setter to validate the size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for _ in range(self.size):
+                print("#" * self.size)
