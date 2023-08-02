2438e9283d715aee40f9e06b28830c9d2ad749ef
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-02 08:57:51 +0200

    square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,8 @@
-"""This class defines a square"""
+"""This defines a square"""
 
 class Square:
+    """This class has a private instance atttribute
+    """
 
     def __init__(self, size):
         self.__size = size
