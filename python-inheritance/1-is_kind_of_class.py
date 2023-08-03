c80f1e84cf2c77eca0175e6fa4067f7d94281e80
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-03 11:20:39 +0200

    Task 1

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,5 @@
+"""This function checks if a object is a nstance or if it is inherited from a class"""
+
+def is_kind_of_class(obj, a_class):
+    """The function uses isinstance to check"""
+    return isinstance(obj,a_class)
