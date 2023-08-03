1519f9c7e98377e5edc9468eab79ede1ae3add35
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-03 14:25:26 +0200

    Task 2

--- python-inheritance/2-inherits_from.py
+++ python-inheritance/2-inherits_from.py
@@ -2,4 +2,5 @@
 
 def inherits_from(obj, a_class):
     """The function checks if the object is inherited directly or indirectly"""
+    
     return isinstance(obj, a_class) and type(obj) != a_class
