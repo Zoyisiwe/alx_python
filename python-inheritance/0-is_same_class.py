cfd0214a87d652c3faf7819e36bc72f0fc98b7e5
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-03 11:03:04 +0200

    Task 0

---
+++ python-inheritance/0-is_same_class.py
@@ -0,0 +1,5 @@
+"""This function checks if the object is exactly an instance of the speified class"""
+
+def is_same_class(obj, a_class):
+    """"The function uses isinsrance to see if the class is exactly like a given object"""
+    return isinstance(obj, a_class)
