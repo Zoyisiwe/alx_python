d2c7b84596ae0147c6b987cce3fe389638ef89da
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-08 14:16:04 +0200

    Area first

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -69,3 +69,7 @@ class Rectangle(Base):
         if value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """This is a public method that adds the function area"""
+        return self.__width * self.__height
