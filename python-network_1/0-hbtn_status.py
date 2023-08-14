c5b9a5f961c21d43a4f4529d35b00c464389a876
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-14 11:38:05 +0200

    Python script

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,20 @@
+"""This is a HTTP request code"""
+
+import requests
+
+def fetch_status():
+    """This is a python script that fetches from the website"""
+    url = "https://alu-intranet.hbtn.io/status"
+    response = requests.get(url)
+    
+    body = {
+        "type": str(type(response.text)),
+        "content": response.text
+    }
+
+    print("Body response:")
+    print("    - type:", body["type"])
+    print("    - content:", body["content"])
+
+if __name__ == "__main__":
+    fetch_status()
---
+++ python-network_1/README.md
@@ -0,0 +1,1 @@
+This project is about HTTPS and HTTP requests and all the other things under this topic
