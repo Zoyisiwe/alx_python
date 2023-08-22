193f474e96d6cd89fd5baa57e49c7c29b8d4c84d
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-22 12:18:38 +0200

    Task 2

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,49 @@
+"""a script that takes in an argument and displays all values"""
+
+import MySQLdb
+import sys
+
+def search_states_by_name(mysql_username, mysql_password, database_name, state_name):
+    """A function that takes 4 arguments"""
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Construct the SQL query with user input
+    query = """
+        SELECT * FROM states
+        WHERE name LIKE %s
+        ORDER BY id ASC
+    """
+
+    # Execute the query with the user input
+    cursor.execute(query, (state_name,))
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Display the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    # Get MySQL credentials and state name from command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    # Call the function to search and list states by name
+    search_states_by_name(mysql_username, mysql_password, database_name, state_name_searched)
