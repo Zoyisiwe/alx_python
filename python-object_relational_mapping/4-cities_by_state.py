8a0c106faeab246a1301f98b79a958cbb83ea13f
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-22 13:58:00 +0200

    Task 4

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,47 @@
+"""a script that lists all cities from the database hbtn_0e_4_usa"""
+
+
+import MySQLdb
+import sys
+
+def list_cities(mysql_username, mysql_password, database_name):
+    # Connect to the MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
+
+    # Create a cursor object to interact with the database
+    cursor = db.cursor()
+
+    # Construct the SQL query
+    query = """
+        SELECT * FROM cities
+        ORDER BY id ASC
+    """
+
+    # Execute the query
+    cursor.execute(query)
+
+    # Fetch all the results
+    results = cursor.fetchall()
+
+    # Display the results
+    for row in results:
+        print(row)
+
+    # Close the cursor and the database connection
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    # Get MySQL credentials from command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    # Call the function to list cities
+    list_cities(mysql_username, mysql_password, database_name)
