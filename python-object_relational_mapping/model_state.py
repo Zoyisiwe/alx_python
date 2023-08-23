18a57370d53ac1140a9ef3c85359e578b499fe10
Zoyisiwe <zoyisiwemakhunga@gmail.com>
2023-08-23 09:57:07 +0200

    models

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+"""a python file that contains the class definition of a State and an instance Base = declarative_base()"""
+
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Database connection
+DATABASE_URI = 'mysql://username:password@localhost:3306/your_database_name'
+engine = create_engine(DATABASE_URI)
+Base = declarative_base()
+
+# State class definition
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
+
+if __name__ == '__main__':
+    # Create tables
+    Base.metadata.create_all(engine)
